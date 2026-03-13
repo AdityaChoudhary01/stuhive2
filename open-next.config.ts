700b5e9dbaa8ec69cf6a036ac90cf8d7460c049c
AdityaChoudhary01 <adityanain552@gmail.com>
2026-03-13 13:25:29 +0530

    Simplify OpenNext configuration for v1.17.1

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,26 +1,3 @@
-// @ts-nocheck
-import cache from "@opennextjs/cloudflare/kvCache";
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
 export default defineCloudflareConfig();
-
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: () => cache,
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-};
-
-export default config;
